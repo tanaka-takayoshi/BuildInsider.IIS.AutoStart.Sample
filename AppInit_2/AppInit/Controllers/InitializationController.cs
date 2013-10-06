b75f80eac7def511f093b170e708a7f845093d3e
tanaka_733 <Takayoshi Tanaka>
2013-10-07 02:00:36 +0900

    AppInit_2 を追加

---
+++ AppInit_2/AppInit/Controllers/InitializationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppInit.Controllers
+{
+    public class InitializationController : Controller
+    {
+        //
+        // GET: /Initialization/
+
+        public ActionResult Index()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            return View();
+        }
+
+    }
+}
